d234dda220936bbfac212997c69cc813bf2702f5
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-30 11:12:02 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/admin
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/root
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -chown root:root /user/root
+HADOOP_USER_NAME=hdfs hdfs dfs -chown admin:admin /user/admin
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /user
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/sensors
+
+flink-yarn-session -tm 2048 -s 2 -d
