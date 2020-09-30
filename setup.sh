8b5b549525afd181888278708f651a8c24fd2be3
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-30 11:12:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,44 @@ HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /user
 HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/sensors
 
 flink-yarn-session -tm 2048 -s 2 -d
+
+echo ""
+echo ""
+echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+echo " Building Schemas"
+echo ""
+echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+echo ""
+echo ""
+
+#
+# Get Schema Registry URL
+curl -X GET "http://edge2ai-1.dim.local:8585/api/v1/admin/schemas/registryInfo" -H "accept: application/json"
+
+echo ""
+echo ""
+
+# Load Schemas into Schema Registry
+# https://registry-project.readthedocs.io/en/latest/schema-registry.html#api-examples
+# http://edge2ai-1.dim.local:7788/swagger
+
+for f in *.avsc
+do 
+
+echo "Uploading Schema File $f"
+schemaname="`echo $f | awk -F'[/_.]' '{print $(NF-1)}'`"
+echo "Schema [$schemaname]"
+echo ""
+
+curl -X POST "http://edge2ai-1.dim.local:7788/api/v1/schemaregistry/schemas" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"type\": \"avro\", \"schemaGroup\": \"Kafka\", \"name\": \"$schemaname\", \"description\": \"schemaname\", \"compatibility\": \"BOTH\", \"validationLevel\": \"LATEST\"}"
+
+echo ""
+
+# Upload body
+
+curl -X POST "http://edge2ai-1.dim.local:7788/api/v1/schemaregistry/schemas/$schemaname/versions/upload?branch=MASTER&disableCanonicalCheck=false" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@/opt/demo/ApacheConAtHome2020/schemas/$schemaname.avsc;type=application/json" -F "description=schemaname"
+
+echo "Added."
+echo ""
+
+done
